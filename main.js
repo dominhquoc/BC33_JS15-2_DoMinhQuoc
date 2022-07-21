83891f4d5c7affbf73a33681ac05fa2f5dd36db7
minhquoc0605 <minhquoc0605@gmail.com>
2022-07-21 15:49:05 +0700

    addd

--- main.js
+++ main.js
@@ -12,76 +12,136 @@
  * Output: Tinh va xuat tien Thue phai tra.
  */
 
-function ex4() {
-  var userName = "Quoc";
-  var total = 41000000;
-  var dependent = 2;
+// function ex4() {
+//   var userName = "Quoc";
+//   var total = 41000000;
+//   var dependent = 2;
 
-  var income = total - 4000000 - dependent * 1600000;
-  console.log(income);
-  result = "";
+//   var income = total - 4000000 - dependent * 1600000;
+//   console.log(income);
+//   result = "";
 
-  if (income < 60000000) {
-    result = income * (5 / 100);
-  } else if (income > 60000000 && income < 120000000) {
-    result = income * (10 / 100);
-  } else if (income > 120000000 && income < 210000000) {
-    result = income * (15 / 100);
-  } else if (income > 210000000 && income < 384000000) {
-    result = income * (20 / 100);
-  } else if (income > 384000000 && income < 624000000) {
-    result = income * (25 / 100);
-  } else if (income > 624000000 && income < 960000000) {
-    result = income * (30 / 100);
-  } else if (income > 960000000) {
-    result = income * (35 / 100);
-  }
-  console.log("Ten khach hang :", userName, ".So % thue phai dong =", result);
-}
-ex4();
+//   if (income < 60000000) {
+//     result = income * (5 / 100);
+//   } else if (income > 60000000 && income < 120000000) {
+//     result = income * (10 / 100);
+//   } else if (income > 120000000 && income < 210000000) {
+//     result = income * (15 / 100);
+//   } else if (income > 210000000 && income < 384000000) {
+//     result = income * (20 / 100);
+//   } else if (income > 384000000 && income < 624000000) {
+//     result = income * (25 / 100);
+//   } else if (income > 624000000 && income < 960000000) {
+//     result = income * (30 / 100);
+//   } else if (income > 960000000) {
+//     result = income * (35 / 100);
+//   }
+//   console.log("Ten khach hang :", userName, ".So % thue phai dong =", result);
+// }
+// ex4();
 
 /**
  * Bai tap nop: Tinh tien Cap
  * Input: MaKhachHang, LoaiKhachHang, SoKetNoi, SoKenhCaoCap
  * Progress: neu Nha dan => TongHoaDon = PhiXuLy1 + PhiCoBan1 + KenhCaoCap1
- * neu Doanh nghiep => KetNoiThem < 10 => PhiCoBan. 
- * Nguoc lai KetNoiThem > 10 => PhiCoBan = PhiCoBan2 + (KetNoiThem - 10)*5 
+ * neu Doanh nghiep => KetNoiThem < 10 => PhiCoBan.
+ * Nguoc lai KetNoiThem > 10 => PhiCoBan = PhiCoBan2 + (KetNoiThem - 10)*5
  * => TongHoaDon = PhiXuLy2 + PhiCoBan + KenhCaoCap2
  * Output: TongHoaDon
  */
 //OPTION
 //Neu la nha dan
-var MaKhachHang1 = 'Quoc';
-var LoaiKhachHang1 = 'NhaDan';
+// var MaKhachHang1 = 'Quoc';
+// var LoaiKhachHang1 = 'NhaDan';
+// var PhiXuLyHoaDon1 = 4.5;
+// var PhiDichVuCoBan1 = 20.5;
+// var ThueKenhCaoCap1 = 7.5;
+// var SoKenhCaoCap1 = 3;
+
+// var TongHoaDon1 = '';
+
+//   TongHoaDon1 = PhiXuLyHoaDon1 + PhiDichVuCoBan1 + ThueKenhCaoCap1*SoKenhCaoCap1;
+//   console.log('MaKhachHang : ',MaKhachHang1,'| Loai Khach Hang:',LoaiKhachHang1,'|',' SoTienPhaiTra = ',TongHoaDon1);
+
+// //Neu la doanh nghiep
+// var MaKhachHang = 'Quoc';
+// var LoaiKhachHang = 'DoanhNghiep';
+// var PhiXuLyHoaDon = 15;
+// var PhiDichVuCoBan = 75;
+// var ThueKenhCaoCap = 50;
+// var SoKenhCaoCap = 2;
+// var PhiDichVuCoBanKetNoiThem= '';
+// var KetNoiThem = 11;
+// var TongHoaDon = '';
+
+// if(KetNoiThem <= 10){
+//   TongHoaDon = PhiXuLyHoaDon + PhiDichVuCoBan + ThueKenhCaoCap*SoKenhCaoCap;
+//   console.log(TongHoaDon);
+// }else if(KetNoiThem > 10){
+//   PhiDichVuCoBanKetNoiThem = 75 + (KetNoiThem - 10)*5;
+//   TongHoaDon = PhiXuLyHoaDon + PhiDichVuCoBanKetNoiThem + ThueKenhCaoCap*SoKenhCaoCap
+//   console.log('MaKhachHang : ',MaKhachHang,'| Loai Khach Hang:',LoaiKhachHang,'|',' SoTienPhaiTra = ',TongHoaDon);
+// }
+
+function myFunction() {
+  var x = document.getElementById("mySelect");
+  var hienthi = document.getElementById("hienthi");
+hienthi.style.display = x.value == "doanhnghiep" ? "block" : "none";
+}
+
+function tinhtoan(){
+var MaKhachHang = document.getElementById("MaKhachHang").value;
+var x = document.getElementById("mySelect");
+var sokenhcaocap = document.getElementById("SoKenhCaoCap").value;
+var hienthi = document.getElementById("hienthi").value;
+
+
+
 var PhiXuLyHoaDon1 = 4.5;
 var PhiDichVuCoBan1 = 20.5;
 var ThueKenhCaoCap1 = 7.5;
-var SoKenhCaoCap1 = 2
 
 
-var TongHoaDon1 = '';
+var TongHoaDon1 = "";
 
-
-  TongHoaDon1 = PhiXuLyHoaDon1 + PhiDichVuCoBan1 + ThueKenhCaoCap1*SoKenhCaoCap1;
-  console.log('MaKhachHang : ',MaKhachHang1,'| Loai Khach Hang:',LoaiKhachHang1,'|',' SoTienPhaiTra = ',TongHoaDon1);
+TongHoaDon1 =
+  PhiXuLyHoaDon1 + PhiDichVuCoBan1 + ThueKenhCaoCap1 * sokenhcaocap;
+console.log(
+  "MaKhachHang : ",
+  MaKhachHang,
+  
+ 
+  " SoTienPhaiTra = ",
+  TongHoaDon1
+);
 
 //Neu la doanh nghiep
-var MaKhachHang = 'Quoc';
-var LoaiKhachHang = 'DoanhNghiep';
+
+
 var PhiXuLyHoaDon = 15;
 var PhiDichVuCoBan = 75;
 var ThueKenhCaoCap = 50;
-var SoKenhCaoCap = 2;
-var PhiDichVuCoBanKetNoiThem= '';
-var KetNoiThem = 11;
-var TongHoaDon = '';
 
-if(KetNoiThem <= 10){
-  TongHoaDon = PhiXuLyHoaDon + PhiDichVuCoBan + ThueKenhCaoCap*SoKenhCaoCap;
-  console.log(TongHoaDon);
-}else if(KetNoiThem > 10){
-  PhiDichVuCoBanKetNoiThem = 75 + (KetNoiThem - 10)*5;
-  TongHoaDon = PhiXuLyHoaDon + PhiDichVuCoBanKetNoiThem + ThueKenhCaoCap*SoKenhCaoCap
-  console.log('MaKhachHang : ',MaKhachHang,'| Loai Khach Hang:',LoaiKhachHang,'|',' SoTienPhaiTra = ',TongHoaDon);
+var PhiDichVuCoBanhienthi = "";
+
+var TongHoaDon = "";
+
+if(x.value == "nhadan"){
+  TongHoaDon1 = PhiXuLyHoaDon1 + PhiDichVuCoBan1 + ThueKenhCaoCap1 * sokenhcaocap;
+  document.getElementById("ketqua").innerHTML = TongHoaDon1;
+
+}else if(x.value == "doanhnghiep"){
+  if (hienthi <= 10) {
+    TongHoaDon = PhiXuLyHoaDon + PhiDichVuCoBan + ThueKenhCaoCap * sokenhcaocap;
+    document.getElementById("ketqua").innerHTML = TongHoaDon;
+  } else if (hienthi > 10) {
+    PhiDichVuCoBanhienthi = 75 + (hienthi - 10) * 5;
+    TongHoaDon =
+      PhiXuLyHoaDon + PhiDichVuCoBanhienthi + ThueKenhCaoCap * sokenhcaocap;
+      document.getElementById("ketqua").innerHTML = TongHoaDon;
+  }
 }
 
+
+
+}
